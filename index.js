6515fa35a117e6eebfb7404a027f103bf7586784
Oleg Solomka <olsolomk@microsoft.com>
2021-01-14 02:48:17 +0000

    tweak index.js file

--- index.js
+++ index.js
@@ -1,57 +1,48 @@
 const tunfd = require('tunfd');
 const fs = require('fs');
+const io = require('socket.io-client');
 
-let iface0 = new tunfd.TunInterface({
+const SOCKET_SERVER_URL = 'wss://85e9e5b54f61.ngrok.io';
+const TUN_NUMBER = 0;
+const ADDRESS = '10.0.0.1'
+
+const iface = new tunfd.TunInterface({
   // optional, kernel will automatically assign a name if not given here
-  name: 'tun0',
+  name: `tun${TUN_NUMBER}`,
   // can be either "tun" or "tap", default is "tun"
   // tun mode gets you ip packets, tap mode gets you ethernet frames
   mode: 'tun',
   // set to true if you want the 4-byte packet information header
   // default is false, which adds IFF_NO_PI to ifr_flags
-  pi: false
+  pi: false,
 });
 
-// // if you want to know the auto-assigned name of the interface
-// console.log(iface.name);
-// // the fd of the new interface
-// console.log(iface.fd);
+console.log('starting the connection');
+try {
+    const socket = io(`${SOCKET_SERVER_URL}/tap-rooms?userId=uid&networkId=nid&address=${ADDRESS}`);
+    socket.on('connect', () => {
+        console.log('>> connect');
 
+        const readStream = fs.createReadStream(null, { fd: iface.fd });
+        const writeStream = fs.createWriteStream(null, { fd: iface.fd });
+
+        readStream.on('data', (packet) => {
+            socket.send(packet);
+        });
+
+        socket.on('event', (data) => {
+            writeStream.write(data);
+        });
+
+        socket.on('disconnect', (data) => {
+            console.log('>> disconnect');
+
+        });
+    });
+} catch (e) {
+    trace.error(e);
+}
+
+
+// prevent nodejs from stopping
 setInterval(() => {}, 100);
-
-
-let iface1 = new tunfd.TunInterface({
-    // optional, kernel will automatically assign a name if not given here
-    name: 'tun1',
-    // can be either "tun" or "tap", default is "tun"
-    // tun mode gets you ip packets, tap mode gets you ethernet frames
-    mode: 'tun',
-    // set to true if you want the 4-byte packet information header
-    // default is false, which adds IFF_NO_PI to ifr_flags
-    pi: false
-  });
-
-
-// let writeStream0 = fs.createWriteStream(null, { fd: iface0.fd });
-let writeStream1 = fs.createWriteStream(null, { fd: iface1.fd });
-
-// how to get packets
-let readStream0 = fs.createReadStream(null, { fd: iface0.fd });
-readStream0.on('data', (packet) => {
-    console.log(packet);
-    writeStream1.write(packet);
-});
-
-// let readStream1 = fs.createReadStream(null, { fd: iface1.fd });
-// readStream1.on('data', (packet) => {
-//     console.log(packet);
-//     writeStream0.write(packet);
-// });
-
-
-// // how to put packets
-// let writeStream = fs.createWriteStream(null, { fd: iface.fd });
-// writeStream.write(...);
-
-// // // fork()
-// // console.log(tunfd.fork());
