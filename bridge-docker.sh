9943cd3a5c6e1a4c8771a0a679d0c9f531d474f3
Oleg Solomka <olsolomk@microsoft.com>
2021-01-20 03:28:45 +0000

    add bridge-docker file

---
+++ bridge-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#################################
+# Set up Ethernet bridge on Linux
+# Requires: bridge-utils
+#################################
+
+# Define Bridge Interface
+br="docker0"
+
+# Define list of TAP interfaces to be bridged,
+# for example tap="tap0 tap1 tap2".
+tap="cstap0"
+
+echo "adding tap interface \"$tap\""
+
+ip tuntap add mode tap $tap
+
+for t in $tap; do
+    echo "adding interface $t to $br"
+    brctl addif $br $t
+done
+
+for t in $tap; do
+    echo "rise the interface $t in promiscous mode"
+    ifconfig $t 0.0.0.0 promisc up
+done
